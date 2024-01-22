8d2bfb126e746bb00ad51aedfcd222d7e031beac
Ain't No Way <tinaojanashley@gmail.com>
2024-01-22 20:33:09 +0800

    Update Forum.js

--- webdevproj/src/Forum.js
+++ webdevproj/src/Forum.js
@@ -5,32 +5,32 @@ import ForumProducts from "./ForumProducts";
 import ForumOrders from "./ForumOrders";
 
 const Forum = ({ user }) => {
-    const navigate = useNavigate();
-    const { id } = useParams();
-  
-    return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/products">Products</Link>
-            </li>
-            <li>
-              <Link to="/orders">Orders</Link>
-            </li>
-          </ul>
-        </nav>
-  
-        <Routes>
-          <Route path="/" element={<ForumHome user={user} />} />
-          <Route path="/products" element={<ForumProducts />} />
-          <Route path="/orders" element={<ForumOrders />} />
-        </Routes>
-      </div>
-    );
-  };
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  export default Forum;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/orders">Orders</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Forumhome user={user} />} />
+        <Route path="/products" element={<ForumProducts />} />
+        <Route path="/orders" element={<ForumOrders />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Forum;
