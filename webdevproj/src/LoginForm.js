6a749303a0b8addd5fc03b84cc4e28a2416772e0
nylRmon <nylraemon.leonardo@tup.edu.ph>
2024-01-22 23:57:12 +0800

    Revert "Update"
    This reverts commit 2678f515c217c119c87d8c5a0df8832908861351.

--- webdevproj/src/LoginForm.js
+++ webdevproj/src/LoginForm.js
@@ -3,15 +3,16 @@ import { useForm } from 'react-hook-form'
 import { FaGoogle } from 'react-icons/fa';
 import { useHistory } from 'react-router-dom';
 
-const LoginForm = () => {
-  const { register, handleSubmit, errors } = useForm();
-  const history = useHistory();
 
-  const onSubmit = (data) => {
-    // Handle form submission here (e.g. send a request to the server)
-    // If the login is successful, redirect the user to the desired page
-    history.push('/dashboard');
-  };
+const LoginForm = () => {
+    const { register, handleSubmit, errors } = useForm();
+    const history = useHistory();
+
+    const onSubmit = (data) => {
+      // Handle form submission here (e.g. send a request to the server)
+      // If the login is successful, redirect the user to the desired page
+      history.push('/dashboard');
+    };
 
   return (
     <div className="container">
